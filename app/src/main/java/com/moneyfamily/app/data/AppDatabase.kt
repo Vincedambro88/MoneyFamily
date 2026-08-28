88eb490727bfbaf9dfebc79bbeb5d02341848ae4
Vincedambro88 <vincenzodambrosio88@gmail.com>
2026-08-28 16:50:59 +0200

    Add Room database

---
+++ app/src/main/java/com/moneyfamily/app/data/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.moneyfamily.app.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [MovementEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun movementDao(): MovementDao
+}
