fb1f7dd0014697d0b6c2d29047875c28008fb2c6
Vincedambro88 <vincenzodambrosio88@gmail.com>
2026-09-21 17:13:57 +0100

    Fix PremiumBilling coroutine cancellation import

--- app/src/main/java/com/moneyfamily/app/PremiumBilling.kt
+++ app/src/main/java/com/moneyfamily/app/PremiumBilling.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.cancel
 import com.android.billingclient.api.BillingClient
 import com.android.billingclient.api.BillingClientStateListener
 import com.android.billingclient.api.BillingFlowParams
